5de6c9aa58ea980d0ce597416268c15cc790c6db
Vyciok <113423064+Vyciok@users.noreply.github.com>
2023-06-23 11:02:37 +0300

    b

---
+++ authController.js
@@ -0,0 +1,18 @@
+class authcontroller {
+  async registraion(req, res) {
+    try {
+    } catch (e) {}
+  }
+
+  async login(req, res) {
+    try {
+    } catch (e) {}
+  }
+  async getUser(req, res) {
+    try {
+      res.json("server work");
+    } catch (e) {}
+  }
+}
+
+module.exports = new authcontroller();
